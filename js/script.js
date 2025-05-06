0bfd934e70dcf80826064e336fb814da12841ee9
Zubair Abdurrasheed <84778198+utilitarianblog@users.noreply.github.com>
2025-05-06 11:42:58 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ function subscribeAlert() {
   alert("Thanks for subscribing to TechSage Journal!");
 }
 function toggleMenu() {
-  const navLinks = document.getElementById("navLinks");
-  navLinks.classList.toggle("active");
+ // const navLinks = document.getElementById("navLinks");
+  // navLinks.classList.toggle("active");
   
   const navWrapper = document.getElementById("navLinks");
   navWrapper.classList.toggle("active");
