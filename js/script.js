96d2f454b1136049d984808f544264a8b5f92104
Zubair Abdurrasheed <84778198+utilitarianblog@users.noreply.github.com>
2025-05-06 11:27:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ function subscribeAlert() {
 function toggleMenu() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("active");
-}
-
-function toggleMenu() {
+  
   const navWrapper = document.getElementById("navLinks");
   navWrapper.classList.toggle("active");
 }
